766bd7f1a55903618ea723674c3f563c78a1e316
Klim Menkov <klim.menkov@ukr.net>
2023-04-14 22:45:33 +0300

    Database snapshot was implemented

--- src/main/java/com/klimmenkov/spring/hibernate/lab_4/controller/admin/AdminMainController.java
+++ src/main/java/com/klimmenkov/spring/hibernate/lab_4/controller/admin/AdminMainController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/adminPage")
@@ -15,5 +18,24 @@ public class AdminMainController {
         model.addAttribute("login", login);
 
         return "admin/admin-account";
+    }
+
+    @PostMapping("/backUpDataBase")
+    public String backUpDataBase() throws IOException, InterruptedException {
+        String command = "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysqldump.exe --user=klim --password=password --port=3307  my_db " +
+                "--result-file=D:\\University\\DataBases\\Lab_4\\Lab_4\\Backups_DB\\backupDB.sql";
+
+        // execute command
+        Process process = Runtime.getRuntime().exec(command);
+        int exitCode = process.waitFor();
+
+        // check if command executed successfully
+        if (exitCode == 0) {
+            System.out.println("Database backup completed successfully.");
+        } else {
+            System.out.println("Database backup failed.");
+        }
+
+        return "redirect:/adminPage";
     }
 }
