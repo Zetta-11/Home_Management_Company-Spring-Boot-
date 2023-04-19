bb24e9f46c24b4f1029078eac84839e6cda8ec78
Klim Menkov <klim.menkov@ukr.net>
2023-04-19 11:11:47 +0300

    Meetings view was fully implemented

--- src/main/java/com/klimmenkov/spring/hibernate/lab_4/controller/tenant/TenantController.java
+++ src/main/java/com/klimmenkov/spring/hibernate/lab_4/controller/tenant/TenantController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -56,5 +57,12 @@ public class TenantController {
         model.addAttribute("allMeetings", allMeetings);
 
         return "tenant/meetings";
+    }
+
+    @GetMapping("meetings/{id}/details")
+    public String getMeetingDetails(@PathVariable(value = "id") Integer id, Model model) {
+        model.addAttribute("details", meetingService.getMeetingDetails(meetingService.getMeeting(id)));
+
+        return "tenant/meeting-details";
     }
 }
