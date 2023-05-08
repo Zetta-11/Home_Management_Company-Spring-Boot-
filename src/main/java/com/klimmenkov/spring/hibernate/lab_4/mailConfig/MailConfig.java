dc059d5a04363f6248229fbcbc95551b08f8f991
Zetta-11 <66167135+Zetta-11@users.noreply.github.com>
2023-05-08 18:09:50 +0300

    Security changes

--- src/main/java/com/klimmenkov/spring/hibernate/lab_4/mailConfig/MailConfig.java
+++ src/main/java/com/klimmenkov/spring/hibernate/lab_4/mailConfig/MailConfig.java
@@ -14,8 +14,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("klim.menkov@gmail.com");
-        mailSender.setPassword("ofedkmhxyidauwwp");
+        mailSender.setUsername("email");
+        mailSender.setPassword("pass");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
